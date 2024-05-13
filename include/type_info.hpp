ee2c4871a7faadc55325ee7e869c3ffea7de2250
shediao.xsd <xushediao1987@163.com>
2024-05-13 22:37:42 +0800

    添加make_variant_t<bool, int, double, string::string>简化创建variant的过程

---
+++ include/type_info.hpp
@@ -0,0 +1,86 @@
+#ifndef _ARGPARSE_TYPE_INFO_
+#define _ARGPARSE_TYPE_INFO_
+#include <map>
+#include <variant>
+namespace argparse {
+
+template <typename... T>
+struct concat_variant;
+
+template <typename... T1>
+struct concat_variant<std::variant<T1...>> {
+  using type = std::variant<T1...>;
+};
+template <typename... T1, typename... T2>
+struct concat_variant<std::variant<T1...>, std::variant<T2...>> {
+  using type = std::variant<T1..., T2...>;
+};
+
+template <typename... T1, typename... T2>
+struct concat_variant<std::variant<T1...>, T2...> {
+  using type =
+      typename concat_variant<std::variant<T1...>,
+                              typename concat_variant<T2...>::type>::type;
+};
+
+template <typename... T>
+using concat_variant_t = typename concat_variant<T...>::type;
+
+template <typename... T>
+struct make_pairs_variant;
+template <typename T>
+struct make_pairs_variant<T> {
+  using type = std::variant<std::pair<T, T>>;
+};
+template <typename T, typename... R>
+struct make_pairs_variant<T, R...> {
+  using type = concat_variant_t<
+      std::variant<std::pair<T, T>, std::pair<T, R>..., std::pair<R, T>...>,
+      typename make_pairs_variant<R...>::type>;
+};
+template <typename... T>
+using make_pairs_variant_t = typename make_pairs_variant<T...>::type;
+
+template <typename... T>
+struct make_map_variant;
+template <typename T>
+struct make_map_variant<T> {
+  using type = std::variant<std::map<T, T>>;
+};
+template <typename T, typename... R>
+struct make_map_variant<T, R...> {
+  using type = concat_variant_t<
+      std::variant<std::map<T, T>, std::map<T, R>..., std::map<R, T>...>,
+      typename make_map_variant<R...>::type>;
+};
+template <typename... T>
+using make_map_variant_t = typename make_map_variant<T...>::type;
+
+template <typename T>
+struct make_vector_variant;
+template <typename... T>
+struct make_vector_variant<std::variant<T...>> {
+  using type = std::variant<std::vector<T>...>;
+};
+template <typename T>
+using make_vector_variant_t = typename make_vector_variant<T>::type;
+
+template <typename... T>
+struct make_variant {
+  using single_variant = std::variant<T...>;
+  using pairs_varint = make_pairs_variant_t<T...>;
+  using map_variant = make_map_variant_t<T...>;
+  using vector_variant =
+      make_vector_variant_t<concat_variant_t<single_variant, pairs_varint>>;
+
+  using type = concat_variant_t<single_variant,
+                                pairs_varint,
+                                map_variant,
+                                vector_variant>;
+};
+
+template <typename... T>
+using make_variant_t = typename make_variant<T...>::type;
+
+}  // namespace argparse
+#endif  // _ARGPARSE_TYPE_INFO_
---
+++ tests/type_info_test.cpp
@@ -0,0 +1,81 @@
+#include "type_info.hpp"
+
+#include <__tuple/tuple_size.h>
+#include <gtest/gtest.h>
+#include <tuple>
+#include <type_traits>
+#include <variant>
+#include "argparse.hpp"
+
+using namespace argparse;
+
+TEST(TypeInfo, ConcatVariant) {
+  static_assert(std::is_same_v<concat_variant_t<std::variant<bool, int>>,
+                               std::variant<bool, int>>);
+  static_assert(
+      std::is_same_v<
+          concat_variant_t<std::variant<bool, int>, std::variant<long, double>>,
+          std::variant<bool, int, long, double>>);
+
+  static_assert(
+      std::is_same_v<
+          concat_variant_t<std::variant<bool, int>, std::variant<long, double>,
+                           std::variant<char, std::string>>,
+          std::variant<bool, int, long, double, char, std::string>>);
+}
+
+TEST(TypeInfo, make_pairs_variant) {
+  using namespace std;
+  static_assert(is_same_v<make_pairs_variant_t<int, bool>,
+                          variant<pair<int, int>, pair<int, bool>,
+                                  pair<bool, int>, pair<bool, bool>>>);
+
+  static_assert(
+      is_same_v<make_pairs_variant_t<int, bool, long>,
+                variant<pair<int, int>, pair<int, bool>, pair<int, long>,
+                        pair<bool, int>, pair<long, int>, pair<bool, bool>,
+                        pair<bool, long>, pair<long, bool>, pair<long, long>>>);
+}
+
+TEST(TypeInfo, make_map_variant) {
+  using namespace std;
+  static_assert(is_same_v<make_map_variant_t<int, bool>,
+                          variant<map<int, int>, map<int, bool>, map<bool, int>,
+                                  map<bool, bool>>>);
+
+  static_assert(
+      is_same_v<make_map_variant_t<int, bool, long>,
+                variant<map<int, int>, map<int, bool>, map<int, long>,
+                        map<bool, int>, map<long, int>, map<bool, bool>,
+                        map<bool, long>, map<long, bool>, map<long, long>>>);
+}
+
+TEST(TypeInfo, make_vector_variant) {
+  using namespace std;
+  static_assert(
+      is_same_v<make_vector_variant_t<std::variant<int, long, double>>,
+                std::variant<std::vector<int>, vector<long>, vector<double>>>);
+}
+
+TEST(TypeInfo, make_variant) {
+  using namespace std;
+  static_assert(
+      std::variant_size_v<make_variant_t<bool, int, double, string>> == 56);
+  static_assert(
+      is_same_v<
+          make_variant_t<bool, int, double>,
+          std::variant<bool, int, double, pair<bool, bool>, pair<bool, int>,
+                       pair<bool, double>, pair<int, bool>, pair<double, bool>,
+                       pair<int, int>, pair<int, double>, pair<double, int>,
+                       pair<double, double>, map<bool, bool>, map<bool, int>,
+                       map<bool, double>, map<int, bool>, map<double, bool>,
+                       map<int, int>, map<int, double>, map<double, int>,
+                       map<double, double>, vector<bool>, vector<int>,
+                       vector<double>, vector<pair<bool, bool>>,
+                       vector<pair<bool, int>>, vector<pair<bool, double>>,
+                       vector<pair<int, bool>>, vector<pair<double, bool>>,
+                       vector<pair<int, int>>, vector<pair<int, double>>,
+                       vector<pair<double, int>>, vector<pair<double, double>>>
+
+          >);
+}
